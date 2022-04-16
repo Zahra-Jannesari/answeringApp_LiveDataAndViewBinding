2b94a2d89e552dfb2285f7d312349a6e6c437349
Zarisa <jannesari.zarisa@gmail.com>
2022-04-16 20:07:56 +0430

    add repository

---
+++ app/src/main/java/com/zarisa/answeringapp_cleanprojectlivedata/Repository.kt
@@ -0,0 +1,11 @@
+package com.zarisa.answeringapp_cleanprojectlivedata
+
+object QuestionRepository {
+//    val questionList = arrayListOf(
+//        Question(" result of 2 + 2 ","4",false),
+//        Question(" result of  5 - 2 ","3",false),
+//        Question(" result of 3 * 4","12",false),
+//        Question(" result of 10 % 2","0",false),
+//        Question(" result of 3 * 5","15",false)
+//    )
+}
